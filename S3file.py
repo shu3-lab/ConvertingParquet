7a5b41dca623dd46252b9763aefea0f3167c1a00
shu3-lab <shushushu912@gmail.com>
2020-07-25 22:42:13 +0900

    modify extend

--- S3file.py
+++ S3file.py
@@ -3,7 +3,7 @@ import boto3
 import pandas as pd
 import datetime
 
-class S3_control:
+class S3_control(abstract_S3):
     def __init__(self):
         super().__init__()
         s3 = boto3.resource('s3')
@@ -13,13 +13,14 @@ class S3_control:
         bucket = self.make_bucket(self.s3,bucketname)
         df = pd.read_csv('s3n://'+bucketname+'/'+filename+'.csv')
         df.to_csv('./csv/'+filename+'.csv', index=False, encoding='utf-8')
-
+    
     def to_S3(self, bucketname, filename):
         bucket = self.make_bucket(self.s3,bucketname)
         now = datetime.datetime.now()
         timestamp = "{0:%Y%m%d%H%M%S}".format(now)
-        bucket.upload_file('./parquet'+filename+'.parquet',timestamp+filename+'.parquet')
+        bucket.upload_file('./parquet/'+filename+'.parquet',timestamp+filename+'.parquet')
     
+    @staticmethod
     def make_bucket(s3,bucketname):
         bucket = s3.Bucket(bucketname)
         return bucket

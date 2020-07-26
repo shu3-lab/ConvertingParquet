0475366e232bebcd9af4c3252c131aeb221eb2bf
shu3-lab <shushushu912@gmail.com>
2020-07-26 18:28:29 +0900

    add skip conditon

--- logic/test_conversion.py
+++ logic/test_conversion.py
@@ -2,6 +2,9 @@ from logic.convertion import convert_parquet as cp
 import os
 import pytest
 
+path = os.path.isdir('./parquet')
+
+@pytest.mark.skipif(path==False, reason='localonly')
 def test_1():
     test_filename = 'TEST'
     expect_path = './parquet/'+test_filename+'.parquet'
